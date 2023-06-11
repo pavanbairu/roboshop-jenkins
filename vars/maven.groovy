bb4d00c443e7182b564a6da76a9286392d4c2326
pavanbairu <pavan.bairu1@gmail.com>
2023-06-11 19:53:25 +0530

    added java binaries

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -20,7 +20,7 @@ def call() {
 
             stage('Code Quality') {
                 steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://3.83.49.238:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries-./target'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://3.83.49.238:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
 //                    sh 'echo Code Quality'
                 }
             }
