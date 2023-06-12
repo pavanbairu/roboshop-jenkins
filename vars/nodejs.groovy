807f19066c749e48cad1800f00f59c0c17dba934
pavanbairu <pavan.bairu1@gmail.com>
2023-06-12 10:32:43 +0530

    created a zip file with node_modules from npm instal,  server.js from cart service and new file for version

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -10,6 +10,10 @@ def call() {
             ansiColor('xterm')
         }
 
+        environment {
+            NEXUS = credentials('NEXUS')
+        }
+
         stages {
 
             stage('Code Quality') {
@@ -45,14 +49,10 @@ def call() {
                     }
                 }
                 steps {
-                    sh 'env'
-                    sh 'echo Release Application'
-                    sh 'curl -v -u admin:admin123 --upload-file server.js ' +
-                            'http://172.31.95.205:8081/repository/cart/server.js'
-//                    sh 'npm install'
-//                    sh 'echo $TAG_NAME >VERSION'
-//                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${schema_dir}'
-//                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.95.205:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                    sh 'npm install'
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${schema_dir}'
+                    sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.95.205:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
 
