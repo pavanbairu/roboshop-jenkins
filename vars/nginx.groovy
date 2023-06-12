1b368d9103a7b024f5e94233d7e794c5a337f5e8
pavanbairu <pavan.bairu1@gmail.com>
2023-06-12 11:34:30 +0530

    created seperate groovy jenkins config file for frontend

---
+++ vars/nginx.groovy
@@ -0,0 +1,64 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+        options {
+            ansiColor('xterm')
+        }
+
+        stages {
+
+            stage('Code Quality') {
+                steps {
+//                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.85.125:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    sh 'echo Code Quality'
+                }
+            }
+
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit tests'
+                    //sh 'npm test'
+                }
+            }
+
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo Checkmarx Scan'
+                }
+            }
+
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo Checkmarx SCA Scan'
+                }
+            }
+
+            stage('Release Application') {
+                when {
+                    expression {
+                        env.TAG_NAME ==~ ".*"
+                    }
+                }
+                steps {
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *'
+                    // Deleting this file as it is not needed.
+                    sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
+                    sh 'curl -f -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http:///172.31.95.205:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+    }
+}
